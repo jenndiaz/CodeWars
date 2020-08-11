8954a0890690262469a591df9943c9e2dd51754e
github username <github email address>
2020-08-11 09:03:13 -0600

    Solve for 8.11.2020 problem

--- index.rb
+++ index.rb
@@ -1,1 +1,6 @@
 # 8.11.2020 Solution "Fill the name of the problem in here"
+def numberToString(num)
+    num.to_s # Convert a Number to a String
+ end
+
+#  Test.expect(numberToString(67) === '67');
